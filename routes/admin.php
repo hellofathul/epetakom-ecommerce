d55a82ccfa1c34a0377e3405f57f8a6f1423e7ee
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-09-13 18:16:42 +0800

    Multi Auth - Create separate route files for admin and vendor

---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+/** Admin Routes */
+
+use App\Http\Controllers\Backend\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
