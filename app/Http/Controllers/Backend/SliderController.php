9488c14754024db6dc6c357346e30a46d58a5424
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-22 13:20:52 +0800

    Slider - Create image upload trait and handling image upload

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
+use App\Traits\ImageUploadTrait;
 use Illuminate\Http\Request;
 
 class SliderController extends Controller
 {
+    use ImageUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -30,7 +32,7 @@ class SliderController extends Controller
     public function store(Request $request)
     {
         $request->validate ([
-            // "banner" => ["required","max:2000", "image"],
+            "banner" => ["required","max:2000", "image"],
             "type" => ["string", "max:200"],
             "title"=> ["required","max:200"],
             "starting_price"=> ["max:200"],
@@ -40,6 +42,12 @@ class SliderController extends Controller
         ]);
 
         $slider = new Slider();
+
+        // Handle file upload
+        $imagePath = $this->uploadImage($request, 'banner', 'uploads');
+
+        $slider->banner = $imagePath;
+
         $slider->type = $request->type;
         $slider->title = $request->title;
         $slider->starting_price = $request->starting_price;
@@ -48,7 +56,7 @@ class SliderController extends Controller
         $slider->status = $request->status;
         $slider->save();
 
-        toastr()->success("Created Successfully!");
+        toastr("Created Successfully!","success");
         return redirect()->back();
     }
 
