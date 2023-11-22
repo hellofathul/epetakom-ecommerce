32f5b7b9c9be5dfc2cf50e2748ed5b73d63205f9
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-22 08:13:06 +0800

    Update Manage Profile part 3

--- app/Http/Controllers/Frontend/UserProfileController.php
+++ app/Http/Controllers/Frontend/UserProfileController.php
@@ -4,10 +4,56 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;   
 
 class UserProfileController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view("frontend.dashboard.profile");
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:200'],
+            'email' => ['required', 'email', 'unique:users,email,' . Auth::user()->id],
+            'phone' => ['max:12'],
+            'image' => ['image', 'max:20000']
+        ]);
+
+        $user = Auth::user();
+
+        // Check if there is an image attached to the request, if doesnt exist then for loop will be ignored
+        if ($request->hasFile('image')) {
+
+            // This loop check if the image attached already existed or not in the app storage,
+            // If the image already exist, the old image will be deleted and replaced with the image in the request
+            if (File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+
+            // This code will get the image in the request
+            $image = $request->image;
+
+            // This code will make sure the every image have unique name, if not, there will be an issue if multiple image have same name
+            $imageName = rand() . '_' . $image->getClientOriginalName();
+
+            // This code will move/upload the image in the request to uploads folder in public folder
+            $image->move(public_path('uploads'), $imageName);
+
+            // This code append path name to the image name so the image path will be stored in the image attributes in database
+            $path = "/uploads/" . $imageName;
+            $user->image = $path;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->save();
+
+        toastr()->success('Profile Updated Successfully', 'Success');
+        return redirect()->back();
     }
 }
--- resources/views/frontend/dashboard/profile.blade.php
+++ resources/views/frontend/dashboard/profile.blade.php
@@ -11,68 +11,70 @@
                         <div class="wsus__dashboard_profile">
                             <div class="wsus__dash_pro_area">
                                 <h4>basic information</h4>
-
-                                
+                                <form action="{{ route('user.profile.update') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('put')
                                     <div class="col-md-12">
                                         <div class="col-md-2">
                                             <div class="wsus__dash_pro_img">
-                                                <img src="{{ asset('frontend/images/ts-2.jpg') }}" alt="img"
-                                                    class="img-fluid w-100">
-                                                <input type="file">
+                                                <img src="{{ Auth::user()->image ? asset(Auth::user()->image) : asset('frontend/images/ts-2.jpg') }}"
+                                                    alt="img" class="img-fluid w-100">
+                                                <input type="file" name="image">
                                             </div>
                                         </div>
 
-                                            <div class="col-md-12 mt-4">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="fas fa-user-tie"></i>
-                                                    <input type="text" placeholder="Name">
-                                                </div>
+                                        <div class="col-md-12 mt-4">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-user-tie"></i>
+                                                <input type="text" placeholder="Name" name="name"
+                                                    value="{{ Auth::user()->name }}">
                                             </div>
-                                            <div class="col-md-12">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="far fa-phone-alt"></i>
-                                                    <input type="text" placeholder="Phone">
-                                                </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="far fa-phone-alt"></i>
+                                                <input type="text" placeholder="Phone" name="phone"
+                                                    value="{{ Auth::user()->phone }}">
                                             </div>
-                                            <div class="col-md-12">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="fal fa-envelope-open"></i>
-                                                    <input type="email" placeholder="Email">
-                                                </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fal fa-envelope-open"></i>
+                                                <input type="email" placeholder="Email" name="email"
+                                                    value="{{ Auth::user()->email }}">
                                             </div>
+                                        </div>
 
                                     </div>
-
                                     <div class="col-xl-12">
                                         <button class="common_btn mb-4 mt-2" type="submit">upload</button>
                                     </div>
-                                    <div class="wsus__dash_pass_change mt-2">
-                                        <div class="row">
-                                            <div class="col-xl-4 col-md-6">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="fas fa-unlock-alt"></i>
-                                                    <input type="password" placeholder="Current Password">
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-4 col-md-6">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="fas fa-lock-alt"></i>
-                                                    <input type="password" placeholder="New Password">
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-4">
-                                                <div class="wsus__dash_pro_single">
-                                                    <i class="fas fa-lock-alt"></i>
-                                                    <input type="password" placeholder="Confirm Password">
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-12">
-                                                <button class="common_btn" type="submit">upload</button>
+                                </form>
+                                <div class="wsus__dash_pass_change mt-2">
+                                    <div class="row">
+                                        <div class="col-xl-4 col-md-6">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-unlock-alt"></i>
+                                                <input type="password" placeholder="Current Password">
                                             </div>
                                         </div>
+                                        <div class="col-xl-4 col-md-6">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-lock-alt"></i>
+                                                <input type="password" placeholder="New Password">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-4">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-lock-alt"></i>
+                                                <input type="password" placeholder="Confirm Password">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-12">
+                                            <button class="common_btn" type="submit">upload</button>
+                                        </div>
                                     </div>
-                                
-
+                                </div>
                             </div>
                         </div>
                     </div>
