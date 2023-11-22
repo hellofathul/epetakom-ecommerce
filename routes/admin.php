6ae7d4b4a94ffab900d6a422a2b9a5e1bb3a1819
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-22 11:55:26 +0800

    Slider - Backend slider page design

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@
 
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -12,3 +13,6 @@ Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard'
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+/** Slider Routes */
+Route::resource('slider', SliderController::class);
