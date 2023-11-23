5a1a1172b6f7516510982d2969b702338db72798
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-23 23:38:45 +0800

    Start working on Product Brand

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@
 
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\SubCategoryController;
@@ -32,3 +33,7 @@ Route::resource('sub-category', SubCategoryController::class);
 Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
 Route::get('get-subcategories', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
 Route::resource('child-category', ChildCategoryController::class);
+
+/** Brand routes */
+Route::put('brand/change-status', [BrandController::class, 'changeStatus'])->name('brand.change-status');
+Route::resource('brand', BrandController::class);
