82fbe7689a567d2e8de8b3c2b6bd5096021cb86d
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-24 01:37:30 +0800

    Product - Creating necessary files and DB columns

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -32,6 +32,8 @@
                 <ul class="dropdown-menu">
                     <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link"
                             href="{{ route('admin.brand.index') }}">Brands</a></li>
+                    <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.brand.index') }}">Products</a></li>
                 </ul>
             </li>
             <li class="dropdown {{ setActive(['admin.vendor-profile.*']) }}">
