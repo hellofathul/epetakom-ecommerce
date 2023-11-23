9b50c02a25ccf97509f047d8043a2eff7478de6a
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-11-24 05:45:41 +0800

    Product - implement product image gallery feature

--- routes/admin.php
+++ routes/admin.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\ProductImageGalleryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -48,3 +49,6 @@ Route::get('product/get-subcategories', [ProductController::class, 'getSubCatego
 Route::get('product/get-child-categories', [ProductController::class, 'getChildCategories'])->name('product.get-child-categories');
 Route::put('product/change-status', [ProductController::class, 'changeStatus'])->name('product.change-status');
 Route::resource('products', ProductController::class);
+
+/** Products image gallery route */
+Route::resource('products-image-gallery', ProductImageGalleryController::class);
